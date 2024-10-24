9a9bb3288615a9b8278b90bd5d8e56018ed36f7f
Hiroki Kondo <kompiro@gmail.com>
2024-10-24 16:34:35 +0900

    fin #15

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -5,22 +5,47 @@ import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import { z } from 'zod';
 
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
+
 const FormSchema = z.object({
     id: z.string(),
-    customerId: z.string(),
-    amount: z.coerce.number(),
-    status: z.enum(['pending', 'paid']),
+    customerId: z.string({
+        invalid_type_error: 'Please seelect a customer',
+    }),
+    amount: z.coerce.number().gt(0, { message: 'Please enter an amount greather than $0.' }),
+    status: z.enum(['pending', 'paid'], {
+        invalid_type_error: 'Please select a status',
+    }),
     date: z.string(),
 });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData: FormData) {
-    const { customerId, amount, status } = CreateInvoice.parse({
+export type State = {
+    errors?: {
+      customerId?: string[];
+      amount?: string[];
+      status?: string[];
+    };
+    message?: string | null;
+  };
+
+export async function createInvoice(prevSate: State, formData: FormData) {
+    const validationFields = CreateInvoice.safeParse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
     });
+
+    if (!validationFields.success) {
+        return {
+            errors: validationFields.error.flatten().fieldErrors,
+            message: 'Missing Fields. Faild to Create Invoice.'
+        };
+    }
+
+    const { customerId, amount, status } = validationFields.data;
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
 
@@ -32,7 +57,7 @@ export async function createInvoice(formData: FormData) {
         ;`;
     } catch (error) {
         return {
-            message: 'Database Error: Failed to create invoice.',
+            message: `Database Error: Failed to create invoice.: ${error}`,
         }
     }
     revalidatePath('/dashboard/invoices');
@@ -41,13 +66,21 @@ export async function createInvoice(formData: FormData) {
 
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function updateInvoice(id: string, formData: FormData) {
-    const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(id: string, prevSate: State, formData: FormData) {
+    const validationFields = UpdateInvoice.safeParse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
     });
 
+    if (!validationFields.success) {
+        return {
+            errors: validationFields.error.flatten().fieldErrors,
+            message: 'Missing Fields. Faild to Create Invoice.'
+        };
+    }
+    
+    const { customerId, amount, status } = validationFields.data;
     const amountInCents = amount * 100;
 
     try {
@@ -58,7 +91,7 @@ export async function updateInvoice(id: string, formData: FormData) {
           `;
     } catch (error) {
         return {
-            message: 'Database Error: Failed to update invoice.',
+            message: `Database Error: Failed to update invoice. ${error}`,
         }
 
     }
@@ -68,14 +101,31 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
-    throw new Error('Failed to Delete Invoice');
-
     try {
         await sql`DELETE FROM invoices WHERE id = ${id}`;
     } catch (error) {
         return {
-            message: "Database Error: Failed to delete invoice."
+            message: `Database Error: Failed to delete invoice. ${error}`,
         }
     }
     revalidatePath('/dashboard/invoices');
 }
+
+export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+  ) {
+    try {
+      await signIn('credentials', formData);
+    } catch (error) {
+      if (error instanceof AuthError) {
+        switch (error.type) {
+          case 'CredentialsSignin':
+            return 'Invalid credentials.';
+          default:
+            return 'Something went wrong.';
+        }
+      }
+      throw error;
+    }
+  }
